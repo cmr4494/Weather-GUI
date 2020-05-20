9b51322180be56f3a9d8e106cb1cffbd37ac9666
Sri Uppalapati <uppalapati.7@osu.edu>
2020-05-20 15:33:21 -0400

    Scraper updating

--- Scraper.py
+++ Scraper.py
@@ -1,23 +1,35 @@
 '''kivy looks like best bet for gui'''
 
 import requests
+import time
 from bs4 import BeautifulSoup, NavigableString
+#gobal variables for temp, cloud condition, temp celcius
+
+#Update fucntion
 
 
 class Scraper:
 
     def __init__(self, url):
         self.page = requests.get(url)
-        soup = BeautifulSoup(self.page.content, 'html.parser')
-        results = soup.find(id="current-conditions-body")
-        temperature_fahrenheit = results.find('p', class_='myforecast-current-lrg').text
-        print(temperature_fahrenheit)
-        temperature_celsius = results.find('p', class_='myforecast-current-sm').text
-        print(temperature_celsius)
-        phrase = results.find('p', class_='myforecast-current').text
-        print(phrase)
+        self.soup = BeautifulSoup(self.page.content, 'html.parser')
+        self.temp_F = 0
+        self.temp_C = 0
+        self.cloud = "" 
 
+
+
+    def getData(self):
+        results = self.soup.find(id="current-conditions-body")
+        self.temp_F = results.find('p', class_='myforecast-current-lrg').text
+        print(self.temp_F)
+        self.temp_C = results.find('p', class_='myforecast-current-sm').text
+        print(self.temp_C)
+        self.cloud = results.find('p', class_='myforecast-current').text
+        print(self.cloud)    
 
 if __name__ == "__main__":
-    scraper = Scraper("https://forecast.weather.gov/MapClick.php?lat=41.467431900000065&lon=-81.53766429999996"
-                      "#.Xrdb0qhKg2w")
+    while(True):
+        scraper = Scraper("https://forecast.weather.gov/MapClick.php?lat=41.467431900000065&lon=-81.53766429999996")
+        scraper.getData()
+        time.sleep(3*60*60)
