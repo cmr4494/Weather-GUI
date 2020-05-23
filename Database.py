dd0a8e5dde00f0bd89897232f125e002a304f016
Sri Uppalapati <uppalapati.7@osu.edu>
2020-05-23 11:54:29 -0400

    Installing required software and testing again

--- Database.py
+++ Database.py
@@ -1,20 +1,11 @@
+
 import pymongo
 
-class Database:
-    def __init__(self):
-        client = pymongo.MongoClient("mongodb://localhost:27017/")
-        database = client["database"]
-        self.collection = database["WeatherInfo"]
+myclient = pymongo.MongoClient("mongodb+srv://Sri:easyaccess@cluster0-1x4ot.mongodb.net/test?retryWrites=true&w=majority")
+mydb = myclient["mydatabase"]
+mycol = mydb["customers"]
 
-    def insert(self, temp_F, temp_C, cloud, date, time):
-        toInsert = {"temp_F": temp_F, "temp_C": temp_C, "cloud conditions": cloud, "date": date, "time": time}
-        x = self.collection.insert_one(toInsert)
-        print(x.inserted_id)
+mydict = { "name": "John", "address": "Highway 37" }
 
-if __name__ == "__main__":
-    database = Database()
-    database.insert(420, 69, "shadyaf", "4/20/69", "4:20:69")
-
-
-
-
+x = mycol.insert_one(mydict)
+print("Hi")
